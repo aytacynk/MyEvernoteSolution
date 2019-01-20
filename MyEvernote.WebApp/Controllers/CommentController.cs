ba6e0d5d5f8190e9445cc72faaca55677c7483df
Aytach <aytacyanik@hotmail.com.tr>
2019-01-20 22:27:14 +0300

    Write commit to relational note card.

--- MyEvernote.WebApp/Controllers/CommentController.cs
+++ MyEvernote.WebApp/Controllers/CommentController.cs
@@ -1,5 +1,6 @@
 ﻿using MyEvernote.BusinessLayer;
 using MyEvernote.Entities;
+using MyEvernote.WebApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -83,7 +84,39 @@ namespace MyEvernote.WebApp.Controllers
             }
 
             return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+        }
 
+        [HttpPost]
+        public ActionResult Create(Comment comment, int? noteid)
+        {
+            ModelState.Remove("CreatedOn");
+            ModelState.Remove("ModifiedOn");
+            ModelState.Remove("ModifiedUsername");
+
+            if (ModelState.IsValid)
+            {
+
+                if (noteid == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Note note = noteManager.Find(x => x.Id == noteid);
+
+                if (note == null)
+                {
+                    return new HttpNotFoundResult();
+                }
+
+                comment.Note = note;
+                comment.Owner = CurrentSession.User;
+
+                if (commentManager.Insert(comment) > 0)
+                {
+                    return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
         }
     }
 }
